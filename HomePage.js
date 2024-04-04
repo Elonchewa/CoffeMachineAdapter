54c0218527c6ce1f5f4e70a2b299e3dd4a3a71a0
AbelAddisu <abeladdisu157@gmail.com>
2024-04-04 15:54:00 -0400

    cursor image added

--- HomePage.js
+++ HomePage.js
@@ -1,18 +1,17 @@
 const main = document.getElementByID('main');
 const head = docunmet.getElementByID('header');
 const StartBrew = document.getElementByID('StartBrew');
-let jogoat = 1;
 
 StartBrew.addEventListener('click', BrewAlert);
 
-let i = 0;
-while (i < 10) {
-$(document).mousemove(function (e) {
+function dogChase() {
+
+  $(document).mousemove(function (e) {
     $("#CoffeeMug").css({
       left: e.pageX,
       top: e.pageY + 25
     });
-});
+  });
 }
 function BrewAlert() {
     console.log('Your Coffee is now brewing');
