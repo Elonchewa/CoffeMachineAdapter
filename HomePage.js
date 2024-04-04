844c775faa33b0b37371f3b2cd9d0f8dd9f5cc1e
AbelAddisu <abeladdisu157@gmail.com>
2024-04-04 15:27:21 -0400

    cursor temp

--- HomePage.js
+++ HomePage.js
@@ -1,11 +1,19 @@
 const main = document.getElementByID('main');
 const head = docunmet.getElementByID('header');
 const StartBrew = document.getElementByID('StartBrew');
-
+let jogoat = 1;
 
 StartBrew.addEventListener('click', BrewAlert);
 
-
+let i = 0;
+while (i < 10) {
+$(document).mousemove(function (e) {
+    $("#CoffeeMug").css({
+      left: e.pageX,
+      top: e.pageY + 25
+    });
+});
+}
 function BrewAlert() {
     console.log('Your Coffee is now brewing');
     
