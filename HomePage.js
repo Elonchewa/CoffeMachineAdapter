728277ec9737252065e11a5d46ced32c8ccf6e03
Bertry <110958802+Nubertry@users.noreply.github.com>
2024-04-08 23:31:19 -0400

    nah id make the coffee video later

--- HomePage.js
+++ HomePage.js
@@ -4,17 +4,21 @@ const StartBrew = document.getElementByID('StartBrew');
 
 StartBrew.addEventListener('click', BrewAlert);
 
-function dogChase() {
-
-  $(document).mousemove(function (e) {
-    $("#CoffeeMug").css({
-      left: e.pageX,
-      top: e.pageY + 25
-    });
-  });
-}
 function BrewAlert() {
+  let x = document.getElementById('CoffeeMug');
+  let displaySetting = x.style.display;
+  if (displaySetting == 'block') {
+    x.style.display = 'none';
+  }
+  else {
+    x.style.display = 'block';
+  }
     console.log('Your Coffee is now brewing');
-    
+    $(document).mousemove(function (e) {
+      $("#CoffeeMug").css({
+        left: e.pageX,
+        top: e.pageY + 25
+      });
+    });
 
 }
