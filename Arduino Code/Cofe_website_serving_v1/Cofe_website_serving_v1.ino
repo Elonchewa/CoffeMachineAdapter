2bea3132fec9c0c23923617070179cda367345d9
Elon Raya <80225196+Elonchewa@users.noreply.github.com>
2024-04-23 21:42:17 -0400

    Update Cofe_website_serving_v1.ino

--- Arduino Code/Cofe_website_serving_v1/Cofe_website_serving_v1.ino
+++ Arduino Code/Cofe_website_serving_v1/Cofe_website_serving_v1.ino
@@ -8,8 +8,8 @@
 ESP32Servo CofeServo; //creates servo object
 AsyncWebServer server(80); //creates server object
 
-char* ssid = "Raya_fam";
-char* psswrd = "Mbye$1234";
+char* ssid = "Network_name";
+char* psswrd = "Password";
 
 
 
